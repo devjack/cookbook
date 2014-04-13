949f03b97860ed8e64fef840b8b5a58ec6966ba6
Jack Skinner <sydnerdrage@gmail.com>
2014-04-13 21:16:56 +1000

    Corrected @link annotations in a few files

--- src/Entity/Food.php
+++ src/Entity/Food.php
@@ -21,7 +21,7 @@ namespace Cookbook\Entity;
  * @package  Cookbook
  * @author   Jack Skinner <sydnerdrage@gmail.com>
  * @license  MIT http://opensource.org/licenses/MIT
- * @link     https://bitbucket.org/sydnerdrage/recipefinder
+ * @link     https://bitbucket.org/sydnerdrage/cookbook
  */
 
 class Food
